2af3419e18c2c67526539301562c9a0f33dc9cbf
avnish986 <avnishgupta986@gmail.com>
2020-09-25 18:45:35 +0530

    [Avnish]Created:welcome to employee wage computation

---
+++ src/com/company_fetch/EmpWageBuilderObj.java
@@ -0,0 +1,10 @@
+package com.company_fetch;
+
+public class EmpWageBuilderObj {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		
+	}
+
+}
