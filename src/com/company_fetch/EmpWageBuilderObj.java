94ac9aa7f7ab80689ce34656c3bd2854b236f256
avnish986 <avnishgupta986@gmail.com>
2020-09-25 19:07:17 +0530

    [Avnish]Modified:uc3 adding part time wage with full time

--- src/com/company_fetch/EmpWageBuilderObj.java
+++ src/com/company_fetch/EmpWageBuilderObj.java
@@ -1,7 +1,8 @@
 package com.company_fetch;
 
 public class EmpWageBuilderObj {
-	public static final int isFullTime=1;
+	public static final int isPartTime=1;
+	public static final int isFullTime=2;
 	public static final int empRatePerHr=20;
 	public String attendance()
 	{
@@ -11,20 +12,22 @@ public class EmpWageBuilderObj {
 		else
 			return "Employee Absent";
 	}
-	public double empFullStatus()
+	public double empFullPartStatus()
 	{
-		double empCheck=Math.floor(Math.random()*10)%2;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		return empCheck;
 	}
 	public int dailyWage()
 	{
 		int empHrs=0, empWage=0;
-		int fullStatus=(int) empFullStatus();
-		if(fullStatus==isFullTime)
+		int fullPartStatus=(int) empFullPartStatus();
+		if(fullPartStatus==isPartTime)
+			empHrs=4;
+			else if(fullPartStatus==isFullTime)
 			empHrs=8;
 			else
 			empHrs=0;
-			empWage= empHrs * empRatePerHr;
+			empWage=empHrs * empRatePerHr;
 			return empWage;
 	}
 	public static void main(String[] args) {
